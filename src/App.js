677dde361948cb591933e4a1e3a4f7a4c87ac112
ChunAlt <chunhay122@gmail.com>
2025-04-01 14:22:13 +0100

    01/04/25-2

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ class App extends React.Component {
             <div className="App">
                 <h1>Gakuen iDOLM@STER Support Card Tier List</h1>
                 <span className="section-explanation">
-                    (Last updated: 21/03/2025)<br />
+                    (Last updated: 01/04/2025)<br />
                     This website is a fork of this <a href="https://euophrys.github.io/uma-tiers/">Uma Musume Tier List website</a>.<br />
                     This tier list only considers stats from Support Abilities. It does not consider Signature quality, Card Events, or stats from Produce Items.<br/>
                 </span>
